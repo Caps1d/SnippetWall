16c60f4f308eb57435d36200111ef9bde15d62da
yegor smertenko <ye.smertenko@gmail.com>
2023-12-14 19:05:47 -0800

    add route handlers

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	log.Print("Home endpoint reached")
 	// since this handles a subtree path, it will match any requests
 	// that start with "/", hence we need to add a check to handle unwanted behaviour
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+	log.Print("Home endpoint reached")
 	w.Write([]byte("Get request to home endpoint"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-
-	w.Write([]byte("Display a specific snippet..."))
+	// getting url query string parameters
+	// we also want to make sure that the id is an int
+	// we parse the str and convert it to an int
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		w.Header().Set("allow-id", ">=1")
+		http.NotFound(w, r) // will send a 404 page not found response
+		return
+	}
+	// write a formatted string response
+	fmt.Fprintf(w, "Displaying snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// we can write "POST" or use constants from net/http
+	if r.Method != http.MethodPost {
+		// adds a custom header Allow: POST
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.Write([]byte("Request Method Not Allowed"))
+		// A shortcut to this is the http.Error helper func
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	w.Write([]byte("Create a new snippet..."))
 }
