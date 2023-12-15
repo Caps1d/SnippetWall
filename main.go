76b3de2974174cf3229cd2d2178f4e6654dea4fe
yegor smertenko <ye.smertenko@gmail.com>
2023-12-14 17:09:24 -0800

    add route handlers

--- main.go
+++ main.go
@@ -8,7 +8,23 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Print("Home endpoint reached")
+	// since this handles a subtree path, it will match any requests
+	// that start with "/", hence we need to add a check to handle unwanted behaviour
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Get request to home endpoint"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -16,6 +32,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting servern on: 4000")
 	err := http.ListenAndServe(":4000", mux)
