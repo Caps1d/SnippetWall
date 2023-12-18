6aa310699cda8bed79a3583ccd6a16203365085b
yegor smertenko <ye.smertenko@gmail.com>
2023-12-17 16:43:37 -0800

    add config settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path"
+
+	"github.com/Caps1d/Lets-Go/internal/config"
 )
 
 func main() {
+	// add command line flags
+	// addr := flag.String("addr", ":4000", "HTTP network address")
+	// Below I am storing the flag values in a config struct for convenience
+	var cfg config.Config
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	// servemux is a router
 	mux := http.NewServeMux()
 
@@ -18,9 +29,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting servern on: 4000")
-	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("Starting servern on: %v", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
+	log.Fatal(err)
 }
