4459f9fbfb72fbbb7ffcf2b1bf4441f0bb94046a
yegor smertenko <ye.smertenko@gmail.com>
2024-02-20 15:28:47 -0800

    switch to method-based routing via julienschmidt httprouter add snippet create form file

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,14 @@ func (app *applicaiton) routes() http.Handler {
 	// mux := http.NewServeMux()
 
 	router := httprouter.New()
+	// enable redirecting with trailing slashes
+	router.RedirectTrailingSlash = true
+
+	// create a handler func which wraps around our notFound helper and assign
+	// as the custom handler for 404 not found responses
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 	// app struct contains config with static dir path
 	fileServer := http.FileServer(http.Dir(path.Clean(app.cfg.StaticDir)))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
