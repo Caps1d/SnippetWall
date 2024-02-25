864cff6681caa2f0716f140b4f7350ac52505aff
yegor smertenko <ye.smertenko@gmail.com>
2024-02-25 15:36:53 -0800

    add tls and switch to https

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -55,6 +55,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = pgxstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	// app struct
 	app := &applicaiton{
@@ -76,7 +77,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting servern on %v", cfg.Addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
