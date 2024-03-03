03b7ee63f0145dfc654cd84b4b61023dc667b68a
yegor smertenko <ye.smertenko@gmail.com>
2024-03-02 18:16:38 -0800

    feat: add cookie storing to test server and disable redirect-following

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -22,12 +23,29 @@ type testServer struct {
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	// initialize a new cookie jar
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the test server client by setting a custom
+	// CheckRedirect function. This function will be called whenever a 3xx
+	// response is received by the client, and by always returning a
+	// http.ErrUseLastResponse error it forces the client to immediately return
+	// the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 	return &testServer{ts}
 }
 
 func (ts *testServer) Get(t *testing.T, urlPath string) (int, http.Header, string) {
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
+	rs, err := ts.Client().Get(ts.URL + urlPath)
 	if err != nil {
 		t.Fatal(err)
 	}
